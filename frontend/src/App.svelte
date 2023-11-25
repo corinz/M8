96d8a50b329cf7cd3135d64cec1200821156281e
corinz <28769409+corinz@users.noreply.github.com>
2023-11-25 15:43:53 -0500

    Feat/multi cluster (#7)
    * feat: multi-cluster querying and display capability and UI (legend) updates to support
    
    * chore: deprecate openAPI and update logger

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -4,16 +4,15 @@
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <!-- Material Icons, Roboto, and Roboto Mono fonts -->
-    <link
-            href="https://fonts.googleapis.com/css2?family=Material+Icons&Roboto+Mono:ital@0;1&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-            rel="stylesheet"
-    />
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Icons&Roboto+Mono:ital@0;1&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"/>
 </svelte:head>
 
 <script lang="ts">
     import SearchBar from "./SearchBar.svelte";
+    import Legend from "./Legend.svelte";
     import {focusedElement, defaultFocus} from "./focus"
-    import { Client, cacheExchange, fetchExchange, setContextClient } from '@urql/svelte';
+    import {Client, cacheExchange, fetchExchange, setContextClient} from '@urql/svelte';
 
     const client = new Client({
         url: 'http://localhost:8080/graphql',
@@ -42,6 +41,13 @@
 
 <main class="nopointer" on:keydown={handleKeyDown}>
     <div>
+<!--        <pre>-->
+<!--  ┌────┐-->
+<!--  │   ▼│-->
+<!--  │ m8 │-->
+<!--  └────┘-->
+<!--        </pre>-->
+        <Legend></Legend>
         <SearchBar searchEventKey={searchEventKey}/>
     </div>
 </main>
---
+++ frontend/src/Legend.svelte
@@ -0,0 +1,103 @@
+<script lang="ts">
+    import {onMount} from 'svelte';
+    import {BaseQuery} from "./gqlQuery";
+    import {gql} from "@urql/svelte";
+    import {activeContextStore, allContextStore} from "./activeContextStore";
+
+    // Cluster is used to display legend items
+    type Cluster = {
+        id: number
+        context: string
+        checked: boolean
+    }
+    // ContextResourceQuery fetches a list of contexts from the api
+    class ContextResourceQuery extends BaseQuery {
+        query = gql`query RootQuery {
+            contexts
+        }`
+    }
+    const contextQuery = new ContextResourceQuery
+    let clusterQStore = contextQuery.executeQuery()
+    let clusters
+
+
+    let activeContextsLocal: Map<string, string> = new Map()
+    let clusterArr: Cluster[] = []
+
+    // execute cluster api query and set store for external use
+    $: if ($clusterQStore.data) {
+        clusters = $clusterQStore.data["contexts"]
+        allContextStore.set(clusters)
+    } else {
+        clusters = null
+    }
+
+    // TODO: is there a better way?
+    // when clusters query is done, populate clusterArr
+    // test length of clusterArr to stop reactive func when length > 0
+    $: if (clusters && clusterArr.length == 0) {
+        for (let i = 0; i < clusters.length; i++) {
+            let clusterObj: Cluster = {
+                id: i + 1,
+                context: clusters[i],
+                checked: false,
+            }
+            clusterArr.push(clusterObj)
+        }
+    }
+
+    // Toggle an active context
+    function toggleCluster(id) {
+        if (id <= clusterArr.length) {
+            let toggle = !clusterArr[id - 1].checked
+            let name = clusterArr[id - 1].context
+            clusterArr[id - 1].checked = toggle
+
+            // update active contexts
+            if (toggle) {
+                activeContextsLocal.set(name, name)
+            } else {
+                activeContextsLocal.delete(name)
+            }
+        }
+        // set activeContexts store so we can subscribe in other places
+        activeContextStore.set(activeContextsLocal)
+    }
+
+    // handleKeyPress toggles clusters based on numpad key presses
+    function handleKeyPress(event) {
+        const key = event.key;
+        if (event.key >= '1' && event.key <= '9') {
+            event.preventDefault()
+            toggleCluster(key)
+        }
+    }
+
+    onMount(() => {
+        window.addEventListener('keydown', handleKeyPress)
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress)
+        }
+    });
+</script>
+
+<div>
+    <fieldset>
+        <legend>Kubernetes Clusters</legend>
+        {#if $clusterQStore.data}
+            <div>
+                {#each clusterArr as {id, context, checked}}
+                    <div>
+                        <input
+                                type="checkbox"
+                                id={id}
+                                aria-label={context}
+                                bind:checked={checked}
+                        />
+                        <label for={id}>{id}: {context}</label>
+                    </div>
+                {/each}
+            </div>
+        {/if}
+    </fieldset>
+</div>
